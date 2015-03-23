fe46a58a9b61a5d427312a1db01514e0090138cb
AIqbal1 <iqbala79@hotmail.com>
2015-03-23 16:10:19 +0000

    fc

--- javascript-object-oriented/js/ch05_start.js
+++ javascript-object-oriented/js/ch05_start.js
@@ -3,7 +3,7 @@ function onReady(){
 
 	var clock = new com.o2GEEK.AlarmClock('clock');
 	var clock2 = new com.o2GEEK.TextClock('clock2',-300,'ETC');
-	var clock2 = new com.o2GEEK.Clock('clock3',300,'X');
+	var clock3 = new com.o2GEEK.Clock('clock3',300,'X');
 
 	//LiveDate.call(clock, 1,2,3);
 	LiveDate.apply(clock,[1,2,3]);
@@ -21,12 +21,25 @@ Date.addToInterval=function (date){
 
 	if(!Date.__interval)
 		Date.__interval = setInterval(function(){Date.updateDates()},1000);
-}
+};
+
+
+// check if its a Date or a function
 Date.updateDates= function(){
 	//console.log(this.__aDates.length);
-	for(var i=0; i<this.__aDates.length;i++)
-		this.__aDates[i].updateSeconds();
-}
+	for(var i=0; i<this.__aDates.length;i++) {
+		if(this.__aDates[i] instanceof Date) {
+			this.__aDates[i].updateSeconds();
+		}
+		else if(this.__aDates[i] instanceof Function) {
+			this.__aDates[i]();
+		}
+		// be valid and have the property value of 'update'
+		else if (this.__aDates[i] && this.__aDates[i]['update']) {
+			this.__aDates[i].update();
+		}
+	}
+};
 
 
 Date.prototype.updateSeconds = function(){
@@ -57,20 +70,35 @@ com.o2GEEK.Clock = function (id,offset,label){
 		this.id = id;
 		this.label= label;
 		 
+//		this.tick(true);
+		this.isTicking = true;
+		var that = this;
+		Date.addToInterval(function() {
+			that.updateClock();
+		});
+};
 
-	var that = this;
-	setInterval(function(){
-		that.updateClock();},1000);
-	this.updateClock();
-}
+com.o2GEEK.Clock.prototype.tick=function(isTick) {	
+//	clearInterval(this.myInternalInterval);
+//	if(isTick) {		
+//		var that = this;
+//		this.myInternalInterval = setInterval(function(){ that.updateClock();},1000);
+//		that.updateClock();
+//	}
+	this.isTicking = isTick;
+
+};
 com.o2GEEK.Clock.prototype.version = '1.00';
 com.o2GEEK.Clock.prototype.updateClock = function(){
 			//console.log(this.version);
-			var date = this.d;
-				//date.updateSeconds();
-			var clock = document.getElementById(this.id);
-			clock.innerHTML = this.formatOutput(date.getHours(),date.getMinutes(),date.getSeconds(),this.label) ;
-		};
+	if(this.isTicking) {
+		var date = this.d;
+		//date.updateSeconds();
+		var clock = document.getElementById(this.id);
+		clock.innerHTML = this.formatOutput(date.getHours(),date.getMinutes(),date.getSeconds(),this.label) ;
+	}
+
+};
 
 com.o2GEEK.Clock.prototype.formatOutput = function(h,m,s,label){
 
@@ -96,14 +124,41 @@ com.o2GEEK.TextClock.prototype.formatOutput = function(h,m,s,label){
 	return this.formatDigits(h) + " Hour " + this.formatDigits(m) +" Minutes "+ this.formatDigits(s) +" Seconds "+ label;
 }
 
-com.o2GEEK.AlarmClock = function(id,offset,label,almH,almM){
+com.o2GEEK.AlarmClock = function(id,offset,label){
 	com.o2GEEK.Clock.apply(this, arguments);
-	this.almH = almH;
-	this.almM = almM;
 	console.log(this.version);
+	
+	this.doUpdate = true;
 	
 	this.dom = document.getElementById(id);
 	this.dom.contentEditable =true;
+	var that = this;
+	this.dom.addEventListener('focus', function() {
+		console.log('im in', this);
+		console.log('im in', this.innerHTML.slice(0, this.innerHTML.lastIndexOf(':')));
+		this.innerHTML = this.innerHTML.slice(0, this.innerHTML.lastIndexOf(':'));
+		that.tick(false);
+	});
+	
+	this.dom.addEventListener('blur', function() {
+		var a = this.innerHTML.split(':');		
+		that.almH = parseInt(a[0]);
+		that.almM = parseInt(a[1]);
+		
+		if((that.almH >=0 && that.almH<24) && 
+				(that.almM >=0 && that.almM<50)) {
+			
+			var event = new Event('restart_tick');
+			this.dispatchEvent(event);
+		}
+		
+		console.log('im out', a);
+		
+	});
+	
+	this.dom.addEventListener('restart_tick', function() { 
+		that.tick(true); 
+	} );
 	
 }
 com.o2GEEK.AlarmClock.prototype = createObject(com.o2GEEK.Clock.prototype, com.o2GEEK.AlarmClock);
@@ -114,9 +169,9 @@ com.o2GEEK.AlarmClock.prototype.formatOutput= function(h,m,s,label){
 		output= 'ALARM WAKE UP';
 		var snd = new Audio('art/beep.mp3');
 			snd.play();
-	}else
+	}else {
 		output= com.o2GEEK.Clock.prototype.formatOutput.apply(this, arguments);
-
+	}
 	return output;
 }
 
